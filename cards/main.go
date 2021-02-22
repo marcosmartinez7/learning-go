7772cdcf0d62d106ffdfc7c7d43fe8a888a68cb4
Marcos Martinez <mmartinezciompi@gmail.com>
2021-02-22 02:32:12 -0300

    save current hand too

--- cards/main.go
+++ cards/main.go
@@ -6,22 +6,29 @@ import (
 )
 
 func main() {
-	currentGameLocation := "currentGame"
+	currentDeckLocation := "currentDeck"
+	currentHandLocation := "currentHand"
+
 	// Initialize deck
 	cards := newDeck()
 	log.Println("[INFO] Full deck size: ", len(cards))
 
 	// Hand dealing
 	log.Println("[INFO] Dealing cards: ")
-	dealingCards := cards.deal(2)
-	dealingCards.print()
+	currentHand := cards.deal(2)
+	currentHand.print()
 	log.Println("[INFO] Current deck ")
 	cards.print()
 	log.Println("[INFO] Current deck size", len(cards))
 
-	// Save current game to file
-	log.Println("[INFO] Saving to file: " + currentGameLocation)
-	gameSaved := cards.saveToFile(currentGameLocation)
-	log.Println("[INFO] Game saved: " + strconv.FormatBool(gameSaved))
+	// Save current game deck to file
+	log.Println("[INFO] Saving current deck to file: " + currentDeckLocation)
+	currentDeckSaved := cards.saveToFile(currentDeckLocation)
+	log.Println("[INFO] Current deck saved: " + strconv.FormatBool(currentDeckSaved))
+
+	// Save current hand to file
+	log.Println("[INFO] Saving current hand to file: " + currentHandLocation)
+	currentHandSaved := currentHand.saveToFile(currentHandLocation)
+	log.Println("[INFO] Current hand saved: " + strconv.FormatBool(currentHandSaved))
 
 }
